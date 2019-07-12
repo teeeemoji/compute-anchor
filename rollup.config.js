cb909084f4e39b4950745356c632705ff0805ff4
张庭岑 <ztc01@baidu.com>
2019-07-12 12:10:43 +0800

    ci: seperate demo and package rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,7 @@
 import pkg from "./package.json";
-import progress from "rollup-plugin-progress";
 import commonjs from "rollup-plugin-commonjs";
 import babel from "rollup-plugin-babel";
-import postcss from "rollup-plugin-postcss";
 import nodeResolve from "rollup-plugin-node-resolve";
-import replace from "rollup-plugin-replace";
 
 export default [
   {
@@ -26,50 +23,6 @@ export default [
       }),
       babel({
         babelrc: true
-      })
-    ]
-  },
-  {
-    /**
-     * to build demo page, and demo page is also for jest test
-     */
-    input: "./demo/src/index.js",
-    output: [
-      {
-        file: "demo/dist/index.js",
-        format: "umd"
-        // sourcemap: 'inline'
-      }
-    ],
-    plugins: [
-      progress(),
-      nodeResolve({
-        browser: true
-      }),
-      commonjs({
-        include: ["node_modules/**"],
-        exclude: [
-          // 'node_modules/process-es6/**'
-        ],
-        namedExports: {
-          "node_modules/react/index.js": [
-            "Children",
-            "Component",
-            "PropTypes",
-            "createElement"
-          ],
-          "node_modules/react-dom/index.js": ["render"],
-          "node_modules/lodash/debounce.js": ["debounce"]
-        }
-      }),
-      babel({
-        babelrc: true
-      }),
-      postcss({
-        extensions: [".css"]
-      }),
-      replace({
-        "process.env.NODE_ENV": JSON.stringify("production")
       })
     ]
   }
